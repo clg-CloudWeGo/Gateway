8b372d661c8fc21c6daf831a538532f0ca837864
曹信 <raccoon@MacBook-Air-2.local>
2023-07-29 22:08:02 +0800

    add etcd for server regist

--- EchoService/main.go
+++ EchoService/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	api "github.com/clg-CloudWeGo/Echo/kitex_gen/api/echo"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 	"log"
 	"net"
 )
 
 func main() {
 	add, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8887")
-	svr := api.NewServer(new(EchoImpl), server.WithServiceAddr(add))
-
+	s := new(EchoImpl)
+	svr := InitEtcdRegistry(s, "EchoService", add)
 	err := svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
+func InitEtcdRegistry(s *EchoImpl, serviceName string, addr *net.TCPAddr) server.Server {
+	r, err := etcd.NewEtcdRegistry([]string{"localhost:2379"})
+	if err != nil {
+		log.Fatal("Error: fail to new etcd registry---" + err.Error())
+	}
+
+	ebi := &rpcinfo.EndpointBasicInfo{
+		ServiceName: serviceName,
+		Tags:        map[string]string{"Cluster": serviceName + "Cluster"},
+	}
+
+	svr := api.NewServer(s, server.WithRegistry(r), server.WithServiceAddr(addr), server.WithServerBasicInfo(ebi))
+	return svr
 }
--- StudentService/main.go
+++ StudentService/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	demo "github.com/Raccoon-njuse/rpcsvr/kitex_gen/demo/studentservice"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 	"log"
 	"net"
 )
@@ -11,10 +13,23 @@ func main() {
 	add, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8889")
 	s := new(StudentServiceImpl)
 	s.InitDB()
-	svr := demo.NewServer(s, server.WithServiceAddr(add))
-
+	svr := InitEtcdRegistry(s, "StudentService", add)
 	err := svr.Run()
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
+func InitEtcdRegistry(s *StudentServiceImpl, serviceName string, addr *net.TCPAddr) server.Server {
+	r, err := etcd.NewEtcdRegistry([]string{"localhost:2379"})
+	if err != nil {
+		log.Fatal("Error: fail to new etcd registry---" + err.Error())
+	}
+
+	ebi := &rpcinfo.EndpointBasicInfo{
+		ServiceName: serviceName,
+		Tags:        map[string]string{"Cluster": serviceName + "Cluster"},
+	}
+
+	svr := demo.NewServer(s, server.WithRegistry(r), server.WithServiceAddr(addr), server.WithServerBasicInfo(ebi))
+	return svr
 }
